9160a0fbc0c6f05063a4262eeb90a41b6f4e9175
cribsb <chrisbogaards.cb@gmail.com>
2015-03-28 16:10:58 +0100

    89

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
 <?php 
 $name = $_FILES['file']['name'];
 
+$extension = strtolower(substr($name, strpos($name, '.') + 1));
+$type = $_FILES['file']['size'];
+$size = $_FILES['file']['size'];
+
 $tmp_name = $_FILES['file']['tmp_name'];
 
 if(isset($name)){
 	if(!empty($name)){
-		$location = 'uploads/';
-		if(move_uploaded_file($tmp_name, $location.$name)){
-			echo 'uploaded';
-		}else{
-			echo 'uh oh....';
-		}
+        if(($extension=='jpg'||$extension=="png")&&($type=='image/jpeg'||$type=='image/png'||$type=='image/jpg')) {
+            $location = 'uploads/';
+            if (move_uploaded_file($tmp_name, $location . $name)) {
+                echo 'uploaded';
+            } else {
+                echo 'uh oh....';
+            }
+        } else {
+            echo 'please chose a jpg/png image';
+        }
 	}else{
 		echo 'please choose a file';
 	}
+} else {
+    echo 'the image has to be jpg/png';
 }
 ?>
 
